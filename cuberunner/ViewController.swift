a106f42f4a28f6ad97c2f62ce0d6b68d40a7aa74
Joey Muia <joseph.b.muia@gmail.com>
2016-04-01 21:11:45 -0400

    Initial Commit

---
+++ cuberunner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cuberunner
+//
+//  Created by Joey Muia on 2016-04-01.
+//  Copyright © 2016 Joey Muia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
