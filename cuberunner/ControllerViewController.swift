affc27220c765f76b84ec6dc30db67c02094ea8f
Joey Muia <joseph.b.muia@gmail.com>
2016-04-03 01:13:38 -0400

    Socket io events   + handlers

--- cuberunner/ControllerViewController.swift
+++ cuberunner/ControllerViewController.swift
@@ -12,22 +12,28 @@ import SocketIOClientSwift
 
 class ControllerViewController: UIViewController {
     @IBOutlet weak var gameIdLabel: UILabel!
+    @IBOutlet weak var tapToStartLabel: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var gameId: String!
+    var gameActive = false
     
-    let socket = SocketIOClient(socketURL: NSURL(string: "http://localhost:8080")!, options: [.Log(true)])
+    let socket = SocketIOClient(socketURL: NSURL(string: "http://localhost:8080")!, options: [.Log(false), .ReconnectAttempts(0)])
     let motionManager = CMMotionManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         gameIdLabel.text = gameId
+        tapToStartLabel.hidden = true
         
-        // TODO - connect to socket + setup handlers
+        addSocketHandlers()
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        
+        socket.connect()
         
         if !motionManager.deviceMotionAvailable {
             let uiAlert = UIAlertController(title: "Device Error", message: "Device is not supported.", preferredStyle: .Alert)
@@ -42,8 +48,61 @@ class ControllerViewController: UIViewController {
             
             let quat = (data?.attitude.quaternion)!
             let pitch = self.rad2deg(atan2(2*(quat.x*quat.w + quat.y*quat.z), 1 - 2*quat.x*quat.x - 2*quat.z*quat.z))
-
-            print (pitch)
+            
+            if (self.gameActive) {
+                self.socket.emit("controller:input", ["id": self.gameId, "value": pitch])
+            }
+        }
+    }
+    
+    func addSocketHandlers() {
+        socket.on("connect") { data, ack in
+            print("socket connected")
+            self.activityIndicator.hidden = true
+            self.tapToStartLabel.hidden = false
+        }
+        
+        socket.on("game:finished") { data, ack in
+            print("game completed")
+        }
+        
+        socket.on("game:notfound") { data, ack in
+            print ("game not found")
+            let uiAlert = UIAlertController(title: "Code Error", message: "Game not found.", preferredStyle: .Alert)
+            uiAlert.addAction(UIAlertAction(title: "Ok", style: .Cancel, handler: { action in self.transitionToMenu() }))
+            self.presentViewController(uiAlert, animated: true, completion: nil)
+            return
+        }
+        
+        socket.on("reconnect") { data in
+            print("reconnect")
+            print(data.0)
+        }
+        
+        socket.on("reconnectAttempt") { data in
+            print("reconnect attempt")
+            print(data.0)
+        }
+        
+        socket.on("error") { data in
+            self.socket.disconnect()
+            self.socket.removeAllHandlers()
+            
+            let uiAlert = UIAlertController(title: "Server Error", message: data.0[0] as? String, preferredStyle: .Alert)
+            uiAlert.addAction(UIAlertAction(title: "Ok", style: .Cancel, handler: { action in self.transitionToMenu() }))
+            self.presentViewController(uiAlert, animated: true, completion: nil)
+        }
+        
+        socket.on("disconnect") { data in
+            print("socket disconnected")
+            print (data.0)
+        }
+    }
+    
+    @IBAction func screenTapped(sender: AnyObject) {
+        if (!gameActive) {
+            socket.emit("controller:start", ["id": self.gameId])
+            gameActive = true
         }
     }
     
